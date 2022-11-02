7223cdc601a5c52d91dc0cc338b375de6b1f272a
Northy007 <k.thaithat007@gmail.com>
2022-11-02 09:43:10 +0700

    duck van

--- 10.Van.py
+++ 10.Van.py
@@ -30,6 +30,7 @@ class Queue :
         def size(self) :
             return len(self.items)
 
+
 class BST:
     def __init__(self):
         self.root = None
@@ -54,39 +55,45 @@ class BST:
                     root = root.right
         self.size += 1
     
-    def Book(self,num): #Level-Order
-        queue = Queue()
-        root = self.root
-        power = 0
-        queue.enQueue(root)
-        while not queue.isEmpty():
-            n = queue.deQueue()
-            if n.day == 0:
-                n.day = num
-                break
-            n.day -= 1
-            if n.left != None :
-                queue.enQueue(n.left)
-            if n.right != None :
-                queue.enQueue(n.right)
-        return n
-
+    # def Book(self,num): #Level-Order
+    #     queue = Queue()
+    #     stack = Stack()
+    #     queue.enQueue(self.root)
+    #     while not queue.isEmpty():
+    #         n = queue.deQueue()
+    #         stack.push(n)
+    #         stack.peek().day = num
+    #         if stack.peek().left != None :
+    #             queue.enQueue(n.left)   
+    #         if stack.peek().right != None :
+    #             queue.enQueue(n.right)
+    #     return 
+    
     def printTree(self, node, level = 0):
         if node != None:
             self.printTree(node.right, level + 1)
             print('     ' * level, node)
             self.printTree(node.left, level + 1)
+
+    # def inOrder(self,root):
+    #     if root != None :
+    #         self.inOrder(root.left)
+    #         if root.day != 0:
+    #             root.day -= 1
+    #         self.inOrder(root.right)
+
+
     
 
 tree = BST()
 inp = input('Enter Input : ').split('/')
 Van = inp[0]
 Custumer = inp[1].split()
-for member in range(1,int(Van) + 1):
-    tree.insert(int(member))
-root = tree.root
-n = 1
-
-for day in  Custumer:
-    print("Customer {0} Booking Van {1} | {2} day(s)".format(n,tree.Book(int(day)),day))
-    n += 1
+# for member in range(1,int(Van) + 1):
+#     tree.insert(int(member))
+# n = 1
+# for day in  Custumer:
+#     tree.insert(int(day))
+    
+# root = tree.root
+# tree.printTree(root)    
